a9b1f1e82fd993536c5e41415b797a148f1048fe
hymiside <vorochaev2004@icloud.com>
2022-08-08 18:46:42 +0500

    add initializer config

---
+++ config.py
@@ -0,0 +1,9 @@
+from typing import Dict
+
+from dotenv import dotenv_values
+
+
+class Config:
+    def __init__(self):
+        config: Dict[str] = dotenv_values(".env")
+        self.token: str = config["BOT_TOKEN"]
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from aiogram import Bot, Dispatcher, executor, types
 
+from config import Config
 
-bot = Bot(token="5564021133:AAESz5jSDpDYdOERnfIHtQGlOOt9sbIpDcc")
+
+config = Config()
+bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands="start")
+@dp.message_handler(commands="start", state="*")
 async def start(message: types.Message):
-    await message.answer()
+    await message.answer("dsf")
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
